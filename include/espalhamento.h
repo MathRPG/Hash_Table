066c85406787dc56b6ad87b6ce395f08664deaaa
MathRPG <maugusilva.santos@gmail.com>
2021-02-28 16:43:48 -0300

    Automatic formatting

--- include/espalhamento.h
+++ include/espalhamento.h
@@ -9,39 +9,39 @@
 
 typedef struct registro
 {
-  char nome[30];
-  char email[50];
+	char nome[30];
+	char email[50];
 } REGISTRO;
 
 typedef enum
 {
-  LIVRE, OCUPADO, REMOVIDO
+	LIVRE, OCUPADO, REMOVIDO
 } EstadoDoSlot;
 
 typedef struct hash
 {
-  REGISTRO *chaves;
-  EstadoDoSlot *estados;
-  unsigned long tamanho;
-  unsigned long ocupados;
-  unsigned short N;
+	REGISTRO* chaves;
+	EstadoDoSlot* estados;
+	unsigned long tamanho;
+	unsigned long ocupados;
+	unsigned short N;
 } HASH;
 
-bool criar_tabela_hash(HASH *tabela);
-bool destruir_tabela_hash(HASH *tabela);
-bool inserir_na_tabela_hash(HASH *tabela, REGISTRO *registro);
-bool apagar_da_tabela_hash(HASH *tabela, REGISTRO *registro);
-bool busca_na_tabela_hash(HASH *tabela, REGISTRO *registro);
+bool criar_tabela_hash(HASH* tabela);
+bool destruir_tabela_hash(HASH* tabela);
+bool inserir_na_tabela_hash(HASH* tabela, REGISTRO* registro);
+bool apagar_da_tabela_hash(HASH* tabela, REGISTRO* registro);
+bool busca_na_tabela_hash(HASH* tabela, REGISTRO* registro);
 
-void ocupacao_da_tabela_hash(HASH *tabela);
+void ocupacao_da_tabela_hash(HASH* tabela);
 
-unsigned long tamanho_da_tabela_hash(HASH *tabela);
-unsigned long registros_na_tabela_hash(HASH *tabela);
-double densidade_da_tabela_hash(HASH *tabela);
-bool expandir_tabela_hash(HASH *tabela);
-bool encolher_tabela_hash(HASH *tabela);
-bool calcula_primo_proximo_2aN(unsigned short N, unsigned long *primo);
+unsigned long tamanho_da_tabela_hash(HASH* tabela);
+unsigned long registros_na_tabela_hash(HASH* tabela);
+double densidade_da_tabela_hash(HASH* tabela);
+bool expandir_tabela_hash(HASH* tabela);
+bool encolher_tabela_hash(HASH* tabela);
+bool calcula_primo_proximo_2aN(unsigned short N, unsigned long* primo);
 
-unsigned long calcular_valor_do_hash(char *chave, unsigned long M);
+unsigned long calcular_valor_do_hash(char* chave, unsigned long M);
 
 #endif //ESPALHAMENTO_ESPALHAMENTO_H
--- src/main.c
+++ src/main.c
@@ -5,169 +5,169 @@
 
 int main()
 {
-  HASH tabela;
-  REGISTRO p, q;
+	HASH tabela;
+	REGISTRO p, q;
 
-  if (!criar_tabela_hash(&tabela))
-  {
-    printf("Erro na criacao da tabela\n");
-    exit(1);
-  }
+	if (!criar_tabela_hash(&tabela))
+	{
+		printf("Erro na criacao da tabela\n");
+		exit(1);
+	}
 
-  ocupacao_da_tabela_hash(&tabela);
+	ocupacao_da_tabela_hash(&tabela);
 
-  // Inserir primeira chave
-  strcpy(p.nome, "Chicoh");
-  strcpy(p.email, "chicoh@ceu.org.br");
+	// Inserir primeira chave
+	strcpy(p.nome, "Chicoh");
+	strcpy(p.email, "chicoh@ceu.org.br");
 
-  if (!inserir_na_tabela_hash(&tabela, &p))
-  {
-    printf("Falha na insercao\n");
-    exit(1);
-  }
-  else
-  {
-    printf("Inserido\n");
-  }
+	if (!inserir_na_tabela_hash(&tabela, &p))
+	{
+		printf("Falha na insercao\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Inserido\n");
+	}
 
-  // Inserir segunda chave
-  strcpy(p.nome, "Joao Grilo");
-  strcpy(p.email, "joaogrilo@ceu.org.br");
+	// Inserir segunda chave
+	strcpy(p.nome, "Joao Grilo");
+	strcpy(p.email, "joaogrilo@ceu.org.br");
 
-  if (!inserir_na_tabela_hash(&tabela, &p))
-  {
-    printf("Falha na insercao\n");
-    exit(1);
-  }
-  else
-  {
-    printf("Inserido\n");
-  }
- 
+	if (!inserir_na_tabela_hash(&tabela, &p))
+	{
+		printf("Falha na insercao\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Inserido\n");
+	}
 
-  // Inserir primeira chave
-  strcpy(p.nome, "Fernando");
-  strcpy(p.email, "fernando@andrade.org.br");
 
-  if (!inserir_na_tabela_hash(&tabela, &p))
-  {
-    printf("Falha na insercao\n");
-    exit(1);
-  }
-  else
-  {
-    printf("Inserido\n");
-  }
+	// Inserir primeira chave
+	strcpy(p.nome, "Fernando");
+	strcpy(p.email, "fernando@andrade.org.br");
 
-  // Inserir chave duplicada para produzir colisao
-  if (!inserir_na_tabela_hash(&tabela, &p))
-  {
-    printf("Falha na insercao\n");
-    exit(1);
-  }
-  else
-  {
-    printf("Inserido\n");
-  }
+	if (!inserir_na_tabela_hash(&tabela, &p))
+	{
+		printf("Falha na insercao\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Inserido\n");
+	}
 
-  ocupacao_da_tabela_hash(&tabela);
+	// Inserir chave duplicada para produzir colisao
+	if (!inserir_na_tabela_hash(&tabela, &p))
+	{
+		printf("Falha na insercao\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Inserido\n");
+	}
 
-  // Recuperando uma chave que existe
-  strcpy(q.nome, "Joao Grilo");
-  if (!busca_na_tabela_hash(&tabela, &q))
-  {
-    printf("Erro na recuperacao\n");
-  }
-  else
-  {
-    printf("O email de %s eh %s\n", q.nome, q.email);
-  }
+	ocupacao_da_tabela_hash(&tabela);
 
-  // Recuperando uma chave que existe
-  strcpy(q.nome, "Fernando");
-  if (!busca_na_tabela_hash(&tabela, &q))
-  {
-    printf("Erro na recuperacao\n");
-  }
-  else
-  {
-    printf("O email de %s eh %s\n", q.nome, q.email);
-  }
+	// Recuperando uma chave que existe
+	strcpy(q.nome, "Joao Grilo");
+	if (!busca_na_tabela_hash(&tabela, &q))
+	{
+		printf("Erro na recuperacao\n");
+	}
+	else
+	{
+		printf("O email de %s eh %s\n", q.nome, q.email);
+	}
 
-  // Recuperando uma chave que nao existe
-  strcpy(q.nome, "Raimundo Nonato");
-  if (!busca_na_tabela_hash(&tabela, &q))
-  {
-    printf("Erro na recuperacao\n");
-  }
-  else
-  {
-    printf("O email de %s eh %s\n", q.nome, q.email);
-  }
+	// Recuperando uma chave que existe
+	strcpy(q.nome, "Fernando");
+	if (!busca_na_tabela_hash(&tabela, &q))
+	{
+		printf("Erro na recuperacao\n");
+	}
+	else
+	{
+		printf("O email de %s eh %s\n", q.nome, q.email);
+	}
 
-  // Removendo uma chave que existe
-  strcpy(q.nome, "Chicoh");
-  if (apagar_da_tabela_hash(&tabela, &q))
-  {
-    printf("%s removido\n", q.nome);
-  }
-  else
-  {
-    printf("%s nao existe na tabela\n", q.nome);
-  }
+	// Recuperando uma chave que nao existe
+	strcpy(q.nome, "Raimundo Nonato");
+	if (!busca_na_tabela_hash(&tabela, &q))
+	{
+		printf("Erro na recuperacao\n");
+	}
+	else
+	{
+		printf("O email de %s eh %s\n", q.nome, q.email);
+	}
+
+	// Removendo uma chave que existe
+	strcpy(q.nome, "Chicoh");
+	if (apagar_da_tabela_hash(&tabela, &q))
+	{
+		printf("%s removido\n", q.nome);
+	}
+	else
+	{
+		printf("%s nao existe na tabela\n", q.nome);
+	}
 
 // Removendo uma chave que existe
-  strcpy(q.nome, "Fernando");
-  if (apagar_da_tabela_hash(&tabela, &q))
-  {
-    printf("%s removido\n", q.nome);
-  }
-  else
-  {
-    printf("%s nao existe na tabela\n", q.nome);
-  }
+	strcpy(q.nome, "Fernando");
+	if (apagar_da_tabela_hash(&tabela, &q))
+	{
+		printf("%s removido\n", q.nome);
+	}
+	else
+	{
+		printf("%s nao existe na tabela\n", q.nome);
+	}
 
 
-  // Recuperando uma chave que existe
-  strcpy(q.nome, "Fernando");
-  if (!busca_na_tabela_hash(&tabela, &q))
-  {
-    printf("Erro na recuperacao\n");
-  }
-  else
-  {
-    printf("O email de %s eh %s\n", q.nome, q.email);
-  }
+	// Recuperando uma chave que existe
+	strcpy(q.nome, "Fernando");
+	if (!busca_na_tabela_hash(&tabela, &q))
+	{
+		printf("Erro na recuperacao\n");
+	}
+	else
+	{
+		printf("O email de %s eh %s\n", q.nome, q.email);
+	}
 
-  // Removendo uma chave que nao existe
-  strcpy(q.nome, "Raimundo Nonato");
-  if (apagar_da_tabela_hash(&tabela, &q))
-  {
-    printf("%s removido\n", q.nome);
-  }
-  else
-  {
-    printf("%s nao existe na tabela\n", q.nome);
-  }
+	// Removendo uma chave que nao existe
+	strcpy(q.nome, "Raimundo Nonato");
+	if (apagar_da_tabela_hash(&tabela, &q))
+	{
+		printf("%s removido\n", q.nome);
+	}
+	else
+	{
+		printf("%s nao existe na tabela\n", q.nome);
+	}
 
-  // mostrando a tabela
-  ocupacao_da_tabela_hash(&tabela);
+	// mostrando a tabela
+	ocupacao_da_tabela_hash(&tabela);
 
-  // Ampliando
-  printf("Expandindo a tabela...\n");
-  expandir_tabela_hash(&tabela);
+	// Ampliando
+	printf("Expandindo a tabela...\n");
+	expandir_tabela_hash(&tabela);
 
-  // mostrando a tabela
-  ocupacao_da_tabela_hash(&tabela);
+	// mostrando a tabela
+	ocupacao_da_tabela_hash(&tabela);
 
-  if (destruir_tabela_hash(&tabela))
-  {
-    printf("Tabela destruida\n");
-  }
-  else
-  {
-    printf("Problema para destruir a tabela\n");
-  }
+	if (destruir_tabela_hash(&tabela))
+	{
+		printf("Tabela destruida\n");
+	}
+	else
+	{
+		printf("Problema para destruir a tabela\n");
+	}
 
-  return 0;
+	return 0;
 }
