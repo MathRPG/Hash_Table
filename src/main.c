da0b74d370ef6462a1263cf677fc8e790fa68a80
MathRPG <maugusilva.santos@gmail.com>
2021-03-01 14:27:17 -0300

    Refactored some tests

--- src/main.c
+++ src/main.c
@@ -5,19 +5,41 @@
 #include "article.h"
 #include "hashtable.h"
 
+void test_empty_hash_table()
+{
+	/*
+	 * Tests properties of an empty hash table
+	 * Initial values - is_empty, count
+	 * Operations - contains, fetch, remove
+	 */
+	
+	HashTable_t * const ht = ht_create();
+	const char* some_doi = "Some_DOI";
+
+	// Initial values
+	assert(ht != NULL);
+	assert(ht_is_empty(ht) == true);
+	assert(ht_count(ht) == 0);
+
+	// All searches must yield 'failure'
+	assert(ht_contains(ht, some_doi) == false);
+	assert(ht_fetch(ht, some_doi) == NULL);
+
+	// Remove must maintain table intact
+	ht_remove(ht, some_doi);
+	assert(ht_is_empty(ht) == true);
+	assert(ht_count(ht) == 0);
+	assert(ht_contains(ht, some_doi) == false);
+	assert(ht_fetch(ht, some_doi) == NULL);
+	
+	ht_delete(ht);
+}
+
 void test_hash_table_single_article()
 {
 	HashTable_t* const ht = ht_create();
 	const char* const doi = "DOI";
 	const char* const other_doi = "Other_DOI";
-
-	// Properties of an empty ht
-	assert(ht != NULL);
-	assert(ht_is_empty(ht) == true);
-	assert(ht_count(ht) == 0);
-	// All searches must yield failure
-	assert(ht_contains(ht, doi) == false);
-	assert(ht_fetch(ht, doi) == NULL);
 
 	// Testing inserting one article
 	Article_t* a = make_article(doi, "Title", "Author", 2000u);
@@ -58,9 +80,22 @@ void test_hash_table_single_article()
 	ht_delete(ht);
 }
 
+void test_hash_table_multiple_articles()
+{
+
+}
+
+void test_hash_table_insert_override()
+{
+
+}
+
 int main(void)
 {
+	test_empty_hash_table();
 	test_hash_table_single_article();
+	test_hash_table_insert_override();
+	test_hash_table_multiple_articles();
 
 	printf("✔ All Tests Passed!");
 
