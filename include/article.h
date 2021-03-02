58da3e936f21ce37f081381f9251c2bce1a56b06
MathRPG <maugusilva.santos@gmail.com>
2021-03-02 09:21:49 -0300

    Refactored article processes

--- include/article.h
+++ include/article.h
@@ -6,10 +6,11 @@
 typedef struct Article_s Article_t;
 
 Article_t* make_article(const char* doi, const char* title, const char* author, unsigned int year);
-void delete_article(Article_t* a);
-bool articles_are_equal(const Article_t* a, const Article_t* b);
-bool article_has_key(const Article_t* article, const char* key);
 Article_t* duplicate_article(const Article_t* original);
+void delete_article(Article_t* a);
+
 const char* key_of(const Article_t* article);
+bool article_has_key(const Article_t* article, const char* key);
+bool articles_are_equal(const Article_t* a, const Article_t* b);
 
 #endif //ARTICLE_H
--- src/article.c
+++ src/article.c
@@ -4,7 +4,7 @@
 
 #include "article.h"
 
-#define MAX_STR_FIELD_LEN 32
+#define MAX_STR_FIELD_LEN 32lu
 
 struct Article_s
 {
@@ -14,18 +14,36 @@ struct Article_s
 	unsigned year;
 };
 
+unsigned long capped_str_len(const char* const str)
+{
+	const unsigned long len = strlen(str) + 1;
+	return len < MAX_STR_FIELD_LEN ? len : MAX_STR_FIELD_LEN;
+}
+
+void copy_values_to_structure(
+		Article_t* const a, const char* const doi, const char* const title,
+		const char* const author, const unsigned int year)
+{
+	strncpy(a->doi, doi, capped_str_len(doi));
+	strncpy(a->title, title, capped_str_len(title));
+	strncpy(a->author, author, capped_str_len(author));
+	a->year = year;
+}
+
 Article_t* make_article(
 		const char* const doi, const char* const title,
 		const char* const author, const unsigned int year)
 {
-	Article_t* a = (Article_t*)malloc(sizeof(Article_t));
-
-	strncpy(a->doi, doi, strlen(doi) + 1);
-	strncpy(a->title, title, strlen(title) + 1);
-	strncpy(a->author, author, strlen(author) + 1);
-	a->year = year;
-
+	Article_t* const a = (Article_t*)malloc(sizeof(Article_t));
+	copy_values_to_structure(a, doi, title, author, year);
 	return a;
+}
+
+Article_t* duplicate_article(const Article_t* const original)
+{
+	Article_t* const copy = (Article_t*)malloc(sizeof(Article_t));
+	copy_values_to_structure(copy, original->doi, original->title, original->author, original->year);
+	return copy;
 }
 
 void delete_article(Article_t* const a)
@@ -33,14 +51,24 @@ void delete_article(Article_t* const a)
 	free(a);
 }
 
+const char* key_of(const Article_t* const article)
+{
+	return article->doi;
+}
+
 bool strings_equal_up_to(const char* const str_a, const char* const str_b, const unsigned len)
 {
 	return strncmp(str_a, str_b, len) == 0;
 }
 
+bool article_has_key(const Article_t* const article, const char* const key)
+{
+	return strings_equal_up_to(article->doi, key, MAX_STR_FIELD_LEN);
+}
+
 bool articles_are_equal(const Article_t* const a, const Article_t* const b)
 {
-	if (!strings_equal_up_to(a->doi, b->doi, MAX_STR_FIELD_LEN))
+	if (!article_has_key(a, key_of(b)))
 		return false;
 	if (!strings_equal_up_to(a->title, b->title, MAX_STR_FIELD_LEN))
 		return false;
@@ -50,26 +78,3 @@ bool articles_are_equal(const Article_t* const a, const Article_t* const b)
 		return false;
 	return true;
 }
-
-bool article_has_key(const Article_t* const article, const char* const key)
-{
-	return strings_equal_up_to(article->doi, key, MAX_STR_FIELD_LEN);
-}
-
-Article_t* duplicate_article(const Article_t* const original)
-{
-	Article_t* const copy = (Article_t*)malloc(sizeof(Article_t));
-
-	strncpy(copy->doi, original->doi, MAX_STR_FIELD_LEN);
-	strncpy(copy->title, original->title, MAX_STR_FIELD_LEN);
-	strncpy(copy->author, original->author, MAX_STR_FIELD_LEN);
-	copy->year = original->year;
-
-	return copy;
-}
-
-const char* key_of(const Article_t* const article)
-{
-	return article->doi;
-}
-
